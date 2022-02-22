b33ba20fe32f9ff085b46129c43a3622200315c4
esdras-santos <esantoz@protonmail.com>
2022-02-22 19:43:06 -0300

    ds

--- src/structures/priority_queue.rs
+++ src/structures/priority_queue.rs
@@ -1,24 +1,24 @@
 use std::collections::HashMap;
 
 
-pub struct PQueue<T>{
+pub struct PQueue<T> where T: std::cmp::Eq + std::hash::Hash + std::cmp::PartialOrd + std::marker::Copy{
     heap: Vec<T>,
     heap_size: u64,
     capacity: u64,
-    track: HashMap<T,Vec<T>>
+    track: HashMap<T,Vec<usize>>
 
 }
 
-impl<T> PQueue<T> {
+impl<T> PQueue<T> where T: std::cmp::Eq + std::hash::Hash + std::cmp::PartialOrd + std::marker::Copy {
     pub fn new(elems: Option<Vec<T>>) -> Self{
         if elems.is_none(){
             PQueue{heap: Vec::new(), heap_size:0, capacity: 0, track: HashMap::new()}    
         }else{
-            let heap = Vec::with_capacity(elems.unwrap().len());
-            let pq: PQueue<T> = PQueue{heap: Vec::new(), heap_size:elems.unwrap().len(), capacity: elems.unwrap().len(),track: HashMap::new()};
+            let heap: Vec<T> = Vec::with_capacity(elems.as_ref().unwrap().len());
+            let mut pq: PQueue<T> = PQueue{heap: Vec::new(), heap_size:elems.as_ref().unwrap().len() as u64, capacity: elems.as_ref().unwrap().len() as u64,track: HashMap::new()};
             for i in 0..pq.heap_size{
-                pq.add_track(elems.unwrap()[i], i);
-                pq.heap.push(elems[i]);
+                pq.add_track(elems.as_ref().unwrap()[i as usize].clone(), i as usize);
+                pq.heap.push(elems.as_ref().unwrap()[i as usize].clone());
             }
             let mut j: u64 = (pq.heap_size/2)-1;
             while  j >= 0{
@@ -31,7 +31,7 @@ impl<T> PQueue<T> {
     }
 
     pub fn is_empty(&self) -> bool{
-        self.heap == 0
+        self.heap_size == 0
     }
 
     pub fn clear(&mut self){
@@ -40,7 +40,7 @@ impl<T> PQueue<T> {
         self.track.clear()
     }
 
-    pub fn size(&self) -> i64{
+    pub fn size(&self) -> u64{
         self.heap_size
     }
 
@@ -49,7 +49,7 @@ impl<T> PQueue<T> {
     }
 
     pub fn poll(&mut self) -> T{
-        self.remove_at(0)
+        self.remove_at(0).unwrap()
     }
 
     pub fn contains(&self, elem: T) -> bool{
@@ -58,14 +58,14 @@ impl<T> PQueue<T> {
 
     pub fn add(&mut self, elem: T){
         if self.heap_size < self.capacity{
-            self.heap[self.heap_size] = elem
+            self.heap[self.heap_size as usize] = elem
         } else {
             self.heap.push(elem);
             self.capacity += 1
         }
 
-        self.add_track(elem, self.heap_size);
-        self.swim(self.heap_size);
+        self.add_track(elem, self.heap_size as usize);
+        self.swin(self.heap_size);
         self.heap_size+=1;
     }
 
@@ -76,9 +76,9 @@ impl<T> PQueue<T> {
         node1 <= node2
     }
 
-    fn swin(&self, k: u64) {
+    fn swin(&mut self,mut k: u64) {
         let mut parent = (k-1) / 2;
-        while k > 0 && less(k, parent){
+        while k > 0 && self.less(k, parent){
             self.swap(parent , k);
             k = parent;
             parent = (k-1) / 2
@@ -86,8 +86,8 @@ impl<T> PQueue<T> {
 
     }
 
-    fn sink(&self, mut k: u64){
-        while(true){
+    fn sink(&mut self, mut k: u64){
+        while true{
             let left = 2*k+1;
             let right = 2*k+2;
             let mut smallest = left;
@@ -112,84 +112,87 @@ impl<T> PQueue<T> {
         self.heap[i as usize] = j_elem;
         self.heap[j as usize] = i_elem;
 
-        self.map_swap(i_elem, j_elem, i, j) 
+        self.swap_track(i_elem, j_elem, i as usize, j as usize);
     }
 
-    pub fn remove(&self, elem: T) -> bool{
+    pub fn remove(&mut self, elem: T) -> bool{
         let index = self.get_track(elem);
 
-        if index != None {
-            self.removeAt(index.unwrap())
+        if index.is_none() {
+            self.remove_at(index.unwrap());
         }
 
         index != None
     }
 
-    fn removeAt(&mut self, i: usize) -> Option<T>{
+    fn remove_at(&mut self, i: usize) -> Option<T>{
         if self.is_empty() {
-            None
+            return None
         }
 
         self.heap_size -= 1;
         let removed_data = self.heap[i];
-        self.swap(i, self.heap_size);
+        self.swap(i.try_into().unwrap(), self.heap_size);
 
         self.heap.remove(self.heap_size as usize);
-        self.remove_track(&removed_data, self.heap_size);
+        self.remove_track(&removed_data, self.heap_size as usize);
 
-        if i == self.heap_size {
+        if i as u64 == self.heap_size {
             return Some(removed_data)
         }
 
         let elem = self.heap[i];
-        self.sink(i);
+        self.sink(i.try_into().unwrap());
         if self.heap[i] == elem {
-            self.swin(i)
+            self.swin(i.try_into().unwrap())
         }
 
         Some(removed_data)
     }
 
     fn add_track(&mut self, value: T, index: usize){
-        let mut set = self.track.get(&value);
+        let mut set = self.track.get(&value).unwrap().clone().to_vec();
 
-        if set.is_none(){
-            let ni = Vec::new();
+        if set.is_empty(){
+            let mut ni = Vec::new();
             ni.push(index);
-            self.track.insert(value, ni)
+            self.track.insert(value, ni);
         }else{
-            set.unwrap().push(index);
-            self.track.insert(value, *set.unwrap())
+            set.push(index);
+            self.track.insert(value, set);
         }
     }
 
-    fn remove_track(&mut self, value: T, index: usize){
-        let set = self.track.get(&value);
-        set.unwrap().remove(index);
-        if set.unwrap.len() == 0{
-            self.track.remove(&value)
+    fn remove_track(&mut self, value: &T, index: usize){
+        let mut set = self.track.get(&value).unwrap().clone().to_vec();
+        set.remove(index);
+        if set.is_empty(){
+            self.track.remove(&value);
         } else{
-            self.track.insert(&value, *set.unwrap())
+            self.track.insert(*value, set);
         }
     }
 
     fn get_track(&self, value: T) -> Option<usize>{
         let set = self.track.get(&value);
         if !set.is_none(){
-            return set.unwrap().last()
+            return Some(*set.unwrap().last().unwrap())
         }
         return None
     }
 
-    fn swap_track(&self, val1: T, val2: T, val1Index: usize, val2Index: usize) {
-        let set1 = self.track.get(&val1).unwrap();
-        let set2 = self.track.get(&val2).unwrap();
+    fn swap_track(&mut self, val1: T, val2: T, val1Index: usize, val2Index: usize) {
+        let mut set1 = self.track.get(&val1).unwrap().clone().to_vec();
+        let mut set2 = self.track.get(&val2).unwrap().clone().to_vec();
 
         set1.remove(val1Index);
         set2.remove(val2Index);
 
         set1.push(val2Index);
         set2.push(val1Index);
+
+        self.track.insert(val1, set1.clone().to_vec());
+        self.track.insert(val2, set2.clone().to_vec());
     }
 }
 
